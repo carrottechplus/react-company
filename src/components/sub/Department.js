b6e9175c92f8e58b8f986a4ecb1b4da717f5289e
carrottechplus <blohzzang@gmail.com>
2023-06-27 13:07:58 +0900

    youtube data fetching

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,9 +1,42 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+/* 
+axios 활용하여 data fetch (브라우저가 하는 일)
+아예 파싱해서 가져옴
+
+가상돔안쪽에서 사용하려면.. 컴포넌트가 마운트가 되어야지 엑시오스를 호출 할 수 있음.
+스테이트에 담아야함..
+거의 공식으로 useEffect, useState 같이 다님
+
+data라는 프로퍼티값으로 가지고다님
+
+컴포넌트 마운트시 외부 데이터 fetching후 state에 저장한 뒤, 반복 렌더링 처리
+*/
 
 function Department() {
+	const [Members, setMembers] = useState([]);
+	useEffect(() => {
+		axios.get(`${process.env.PUBLIC_URL}/DB/members.json`).then((data) => {
+			// console.log(data.data.members);
+			setMembers(data.data.members);
+		});
+	}, []);
 	return (
 		<Layout name={'Department'}>
-			<p>회사 조직소개 페이지 입니다.</p>
+			{Members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+						</div>
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,7 +1,45 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Youtube() {
-	return <Layout name={'Youtube'}></Layout>;
+	const [Video, setVideo] = useState([]);
+
+	useEffect(() => {
+		const key = 'AIzaSyC4TpEbx2d9lOtjiVQIg3b6wA6ZKKrDL7c';
+		const list = 'PLQZTVbf9_qAn_Nwrz2maZG64AaEBcFZfb';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
+
+		axios.get(`${url}`).then((data) => {
+			console.log(data.data.items);
+			setVideo(data.data.items);
+		});
+	}, []);
+	return (
+		<Layout name={'Youtube'}>
+			{Video.map((video, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={video.snippet.thumbnails.standard.url} alt={video.snippet.title} />
+						</div>
+						<h2>{video.snippet.title}</h2>
+						<p>{video.snippet.description}</p>
+						<span>{video.snippet.videoOwnerChannelTitle}</span>
+						<span>{video.snippet.publishedAt}</span>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
+// const title = data.snippet.title;
+// const desc = data.snippet.description;
+// const name = data.snippet.videoOwnerChannelTitle;
+// const date = data.snippet.publishedAt;
+
+// const channelId = data.snippet.resourceId.videoId;
+// const thumbUrl = data.snippet.thumbnails.standard.url;
 export default Youtube;

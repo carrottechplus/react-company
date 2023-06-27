31a895a0f7bde7bec40c03de2bad83b628cbb10e
carrottechplus <blohzzang@gmail.com>
2023-06-27 11:23:21 +0900

    layout에 useEffect, useRef 작업

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,6 +1,13 @@
+import { useEffect, useRef } from 'react';
+
 function Layout({ name, children }) {
+	const frame = useRef(null);
+	useEffect(() => {
+		// 마운트 됐을때 class="on" 추가 (unmount 됐을때 제거는 꼭 할필요 없다)
+		frame.current.classList.add('on');
+	}, []);
 	return (
-		<section className={`content ${name}`}>
+		<section className={`content ${name}`} ref={frame}>
 			<figure></figure>
 
 			<div className='inner'>

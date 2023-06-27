3e9de2c35dabf218ad05936d52ad1f3bdde7beac
carrottechplus <blohzzang@gmail.com>
2023-06-27 11:56:45 +0900

    반응형 스타일 작업

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,3 +1,5 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, NavLink } from 'react-router-dom';
 
 function Header({ type }) {
@@ -41,6 +43,8 @@ function Header({ type }) {
 					</NavLink>
 				</li>
 			</ul>
+
+			<FontAwesomeIcon icon={faBars} />
 		</header>
 	);
 }

706db1832dfe6aaaff2c1453369f827f7e1481a4
carrottechplus <blohzzang@gmail.com>
2023-06-29 12:04:27 +0900

    base값까지 적용된 커스텀 스크롤 모션

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,8 +1,20 @@
 function Pics({ Scrolled, Pos }) {
-	console.log(Scrolled);
+	const currentPos = Scrolled - Pos;
+	const base = window.innerHeight / 3;
+	const modifiedPos = currentPos + base;
+	// console.log(Scrolled); 이건 스크롤될 때 마다 계속 재호출되는데 그게맞음. 왜냐하면 강제로 줄여버리면..모션이 끊길것..
 	return (
 		<section id='pics' className='myScroll'>
-			<h1 style={{ transform: `translateX(${Scrolled - Pos}px)` }}>FLICKR</h1>
+			<h1 style={{ transform: `translateX(${currentPos}px)` }}>FLICKR</h1>
+
+			<article
+				style={{
+					transform: `translate(-50%, -50%) rotate(${Scrolled >= Pos - base ? modifiedPos : 0}deg) scale(${
+						Scrolled >= Pos - base ? 1 + modifiedPos / 500 : 1
+					})`,
+					opacity: `${Scrolled >= Pos - base ? 1 - modifiedPos / 500 : 1}`,
+				}}
+			></article>
 		</section>
 	);
 }

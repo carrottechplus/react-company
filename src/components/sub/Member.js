20ed45b989de892e77aacbc5e1a632fcce36b52c
carrottechplus <blohzzang@gmail.com>
2023-07-03 10:56:55 +0900

    현재 입력하고 있는 input항목의 name, value값을 실시간으로 기존 State객체에 덮어쓰기

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -20,13 +20,25 @@ function Member() {
 		setVal({ ...Val, [name]: value }); //변경함수 호출해서
 	};
 
+	const check = (value) => {
+		//인수로 현재 State 값을 전달받아서 인증 통과시 true, 실패시 false를 반환하는 로직
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('현재 state 값', Val);
+
+		//check가 true 값을 반환하면 인증통과 그렇지 않으면 인증 실패
+		console.log(check(Val));
+	};
+
 	useEffect(() => {
 		console.log(Val);
 	}, [Val]);
 
 	return (
 		<Layout name={'Member'}>
-			<form action=''>
+			<form action='' onSubmit={handleSubmit}>
 				<fieldset>
 					<legend className='hidden'>회원가입 폼 양식</legend>
 					<table>

7a479c5cd8989a71a1bb7ed0f7d7b2ec847b4872
carrottechplus <blohzzang@gmail.com>
2023-07-04 10:14:43 +0900

    community 페이지 레이아웃 세팅

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,40 @@
 import Layout from '../common/Layout';
+import { useState, useEffect, useRef } from 'react';
 
 function Community() {
-	return <Layout name={'Community'}></Layout>;
+	const input = useRef(null);
+	const textarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	const resetForm = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
+
+	const createPost = () => {
+		setPosts([...Posts, { title: input.current.value, content: textarea.current.value }]);
+		resetForm();
+	};
+
+	//post가 바뀔때마다
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
+
+	return (
+		<Layout name={'Community'}>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요.' ref={input} />
+				<br />
+				<textarea cols='30' rows='3' placeholder='본문을 입력하세요.' ref={textarea}></textarea>
+				<br />
+				<button type='button'>cancel</button>
+				<button type='button' onClick={createPost}>
+					write
+				</button>
+			</div>
+		</Layout>
+	);
 }
 
 export default Community;

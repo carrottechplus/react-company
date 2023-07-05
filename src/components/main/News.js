f57ef9c9d3f395f3a1588c6b5657a05469b34dca
carrottechplus <blohzzang@gmail.com>
2023-07-05 10:54:36 +0900

    메인 페이지 컨텐츠 미리보기 출력

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
 function News() {
+	const getLocalData = () => {
+		const data = localStorage.getItem('post');
+		return JSON.parse(data); // data 문자값을 json 형태로 파싱
+	};
+	const [Posts] = useState(getLocalData);
 	return (
 		<section id='news' className='myScroll'>
-			<article></article>
-			<article></article>
-			<article></article>
-			<article></article>
+			{Posts.map((post, idx) => {
+				if (idx >= 4) return null;
+				return (
+					<article key={idx}>
+						<h3>{post.title}</h3>
+						<p>{post.content}</p>
+					</article>
+				);
+			})}
 		</section>
 	);
 }

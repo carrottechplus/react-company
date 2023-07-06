8dae64e8e829bdb146fc3d3bd1bea870c414aefb
carrottechplus <blohzzang@gmail.com>
2023-07-06 09:57:41 +0900

    모든 이미지 소스가 로딩완료될때까지 로딩 이미지 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,15 +1,18 @@
 import Layout from '../common/Layout';
 import Masonry from 'react-masonry-component';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
+	const frame = useRef(null);
+	const counter = useRef(0);
 	const [Items, setItems] = useState([]);
+	const [Loader, setLoader] = useState(true); // true 로딩바가 맨처음에 보여야하니까
 
 	const getFlickr = async (opt) => {
 		const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1`;
 		const key = '6c70577e2661042cd0ab587b17f6c944';
-		const num = 100;
+		const num = 50;
 		// const myID = '198484213@N03';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_search = 'flickr.photos.search';
@@ -22,9 +25,28 @@ function Gallery() {
 		if (opt.type === 'user')
 			url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
-		const result = await axios.get(url);
-		console.log(result.data.photos.photo);
+		const result = await axios.get(url); //동기화
+		// console.log(result.data.photos.photo);
 		setItems(result.data.photos.photo);
+
+		// 외부 데이터가 State에 담기고 DOM이 생성되는 숭간 모든 img 요소를 찾아서 반복 처리
+		const imgs = frame.current.querySelectorAll('img');
+		imgs.forEach((img, idx) => {
+			// 이미지요소에 load 이벤트가 발생할 때 (소스 이미지까지 로딩이 완료될 때 마다)
+			img.onload = () => {
+				// 내부적으로 카운터값을 1씩 증가
+				++counter.current;
+				console.log(counter);
+
+				// 로딩 완료된 이미지 수와 전체 이미지 수 *2가 같아질 때
+				if (counter.current === num * 2) {
+					// loader 제거하고 이미지 갤러리에 on class 추가
+					setLoader(false);
+
+					frame.current.classList.add('on');
+				}
+			};
+		});
 	};
 
 	useEffect(() => getFlickr({ type: 'interest' }), []);
@@ -33,7 +55,7 @@ function Gallery() {
 
 	return (
 		<Layout name={'Gallery'}>
-			<div className='frame'>
+			<div className='frame' ref={frame}>
 				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
 					{Items.map((item, idx) => {
 						return (
@@ -60,6 +82,7 @@ function Gallery() {
 					})}
 				</Masonry>
 			</div>
+			{Loader && <img className='loader' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alg='loader' />}
 		</Layout>
 	);
 }

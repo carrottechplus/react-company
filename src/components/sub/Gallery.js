85c0cfe1fbe7ff0525036e86d77bab8ffa6c3916
carrottechplus <blohzzang@gmail.com>
2023-07-06 12:04:27 +0900

    검색함수 추가 및 키보드 이벤트 연결

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,23 +4,25 @@ import axios from 'axios';
 import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
+	const searchInput = useRef(null);
 	const btnSet = useRef(null);
 	const enableEvent = useRef(true);
 	const frame = useRef(null);
 	const [Items, setItems] = useState([]);
-	const [Loader, setLoader] = useState(true); // true 로딩바가 맨처음에 보여야하니까
+	const [Loader, setLoader] = useState(true);
 
 	const getFlickr = async (opt) => {
-		let counter = 0; //state에 담으면 계속 재랜더링 그러므로 useRef썻는데,, 값이 사라지는게 아니라서 초기화가 필요함.
+		let counter = 0;
 		const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1`;
 		const key = '6c70577e2661042cd0ab587b17f6c944';
-		const num = 60;
+		const num = 50;
 		// const myID = '198484213@N03';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_search = 'flickr.photos.search';
 		const method_user = 'flickr.people.getPhotos';
 
 		let url = '';
+
 		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
 		if (opt.type === 'search')
 			url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
@@ -60,6 +62,42 @@ function Gallery() {
 		frame.current.classList.remove('on');
 	};
 
+	const showInterest = (e) => {
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+		if (e.target.classList.contains('on')) return;
+
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'interest' });
+	};
+
+	const showMine = (e) => {
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+		if (e.target.classList.contains('on')) return;
+
+		//기존 갤러리 초기화 함수 호출
+		resetGallery(e);
+
+		//새로운 데이터로 갤러리 생성 함수 호출
+		getFlickr({ type: 'user', user: '198484213@N03' });
+	};
+
+	const showSearch = (e) => {
+		const tag = searchInput.current.value.trim(); //글자 양옆에 공백없는지 확인
+		if (tag === '') return alert('검색어를 입력하세요.');
+		//재이벤트, 모션중 재이벤트 방지
+		if (!enableEvent.current) return;
+
+		resetGallery(e);
+		getFlickr({ type: 'search', tags: tag });
+
+		searchInput.current.value = '';
+	};
+
 	// useEffect(() => getFlickr({ type: 'interest' }), []);
 	// useEffect(() => getFlickr({ type: 'search', tags: 'landscape' }), []);
 	useEffect(() => getFlickr({ type: 'user', user: '198484213@N03' }), []);
@@ -67,28 +105,22 @@ function Gallery() {
 	return (
 		<Layout name={'Gallery'}>
 			<div className='btnSet' ref={btnSet}>
-				<button
-					onClick={(e) => {
-						if (!enableEvent.current) return; //모션중이면 return 으로 끊음.
-						if (e.target.classList.contains('on')) return;
-						resetGallery(e);
-						getFlickr({ type: 'interest' });
-					}}
-				>
-					Interest Gallery
-				</button>
-				<button
-					className='on'
-					onClick={(e) => {
-						if (!enableEvent.current) return;
-						if (e.target.classList.contains('on')) return;
-						resetGallery(e);
-						getFlickr({ type: 'user', user: '198484213@N03' });
-					}}
-				>
+				<button onClick={showInterest}>Interest Gallery</button>
+				<button className='on' onClick={showMine}>
 					my Gallery
 				</button>
 			</div>
+
+			<div className='searchBox'>
+				<input
+					type='text'
+					placeholder='검색어를 입력하세요.'
+					ref={searchInput}
+					onKeyPress={(e) => e.key === 'Enter' && showSearch(e)}
+				/>
+				<button onClick={showSearch}>Search</button>
+			</div>
+
 			<div className='frame' ref={frame}>
 				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
 					{Items.map((item, idx) => {

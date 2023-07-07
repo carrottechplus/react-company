d7109cae0425c36d51ee32f83c658c750b905e67
carrottechplus <blohzzang@gmail.com>
2023-07-07 12:47:39 +0900

    frame-motion 적용 및 하위 메뉴 생성

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,6 +1,9 @@
 import { forwardRef, useState, useImperativeHandle } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { Link, NavLink } from 'react-router-dom';
 
 const Menu = forwardRef((props, ref) => {
+	const active = { color: 'aqua' };
 	const [Open, setOpen] = useState(false);
 
 	useImperativeHandle(ref, () => {
@@ -9,13 +12,52 @@ const Menu = forwardRef((props, ref) => {
 		};
 	});
 	return (
-		<>
+		<AnimatePresence>
 			{Open && (
-				<nav id='mobilePanel'>
-					<h1>LOGO</h1>
-				</nav>
+				<motion.nav
+					id='mobilePanel'
+					initial={{ x: -280 }}
+					animate={{ x: 0, transition: { duration: 0.3 } }}
+					exit={{ opacity: 0, x: -280, transition: { duration: 0.3 } }}
+				>
+					<h1>
+						<Link to='/'>LOGO</Link>
+					</h1>
+					<ul id='gnbMo'>
+						<li>
+							<NavLink to='/department' activeStyle={active}>
+								Department
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeStyle={active}>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeStyle={active}>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube' activeStyle={active}>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/contact' activeStyle={active}>
+								Contact
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/member' activeStyle={active}>
+								Member
+							</NavLink>
+						</li>
+					</ul>
+				</motion.nav>
 			)}
-		</>
+		</AnimatePresence>
 	);
 });
 

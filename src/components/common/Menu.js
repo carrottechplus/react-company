5dbd697e719a54aa84c963139f850553c6a78ab3
carrottechplus <blohzzang@gmail.com>
2023-07-07 11:52:57 +0900

    Menu컴포넌트 생성 및 forwardRef처리

---
+++ src/components/common/Menu.js
@@ -0,0 +1,14 @@
+import { forwardRef, useState, useImperativeHandle } from 'react';
+
+const Menu = forwardRef((props, ref) => {
+	const [Open, setOpen] = useState(false);
+
+	useImperativeHandle(ref, () => {
+		return {
+			toggle: () => setOpen(!Open),
+		};
+	});
+	return <nav>Menu</nav>;
+});
+
+export default Menu;

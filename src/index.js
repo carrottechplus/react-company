ea27f484ad25cc21407facb5c997f43fef032acc
carrottechplus <blohzzang@gmail.com>
2023-07-11 12:56:26 +0900

    전역 store데이터를 루트 컴포넌트인 App에 전달

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 //브라우저 라우터를 쓰는게 맞는데 깃허브에 올리는 제한사항으로 hashrouter를 씀.
 ReactDOM.render(
 	<HashRouter>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</HashRouter>,
 	document.getElementById('root')
 );
 
+// redux 시작 (react18버전에서는 x)
 // npm i redux
 // npm i react-redux

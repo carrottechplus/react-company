b9ee625a5db3f9d90403199e446a782d6915a3c4
carrottechplus <blohzzang@gmail.com>
2023-07-12 09:49:09 +0900

    버튼에 setMembers 연결

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,11 +1,23 @@
 import Layout from '../common/Layout';
 import { useSelector } from 'react-redux';
+import { setMembers } from '../../redux/action';
 
 function Department() {
 	const Members = useSelector((store) => store.memberReducer.members);
 
 	return (
 		<Layout name={'Department'} txt={'Hello-World'}>
+			<button
+				onClick={() => {
+					// 리액트에선 불변성' 특징떄문에 데이터 변경하려면 원본 유지 후 deep copy하여 작업해야함.
+					const newMembers = [...Members];
+					newMembers[0].name = 'Emma';
+					const newAction = setMembers(newMembers);
+					console.log(newAction);
+				}}
+			>
+				멤버 데이터 변경
+			</button>
 			{Members.map((member, idx) => {
 				return (
 					<article key={idx}>

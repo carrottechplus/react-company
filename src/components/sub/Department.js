963d1e48d9a62a31e18df27461e6ecc0618c347b
carrottechplus <blohzzang@gmail.com>
2023-07-12 10:03:49 +0900

    department 멤버 데이터 변경 버튼 작업 설명

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -11,11 +11,14 @@ function Department() {
 			<button
 				onClick={() => {
 					// 리액트에선 불변성' 특징떄문에 데이터 변경하려면 원본 유지 후 deep copy하여 작업해야함.
+					// 버튼 클릭 시 기존 State값을 Deep copy
 					const newMembers = [...Members];
+					// Deep copy된 참조형 자료 State정보값을 변경
 					newMembers[0].name = 'Emma';
+					// action 생성함수의 인수로 넣어 새로운 action 객체 생성
 					const newAction = setMembers(newMembers);
-					dispatch(newAction); //action에서 reducer로 보내주는거, dispatch 안하면 화면에 바뀌진않음.
-					console.log(newAction);
+					// 그렇게 만들어진 action 객체를 dispatch를 통해 reducer에 전달 (dispatch 안하면 화면에 바뀌진않음)
+					dispatch(newAction);
 				}}
 			>
 				멤버 데이터 변경

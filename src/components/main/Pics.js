5ea1132d23b23c5161918a796b6b88427534671d
carrottechplus <blohzzang@gmail.com>
2023-07-13 09:32:17 +0900

    main페이지에 pics 모션 연습

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -5,16 +5,19 @@ function Pics({ Scrolled, Pos }) {
 	// console.log(Scrolled); 이건 스크롤될 때 마다 계속 재호출되는데 그게맞음. 왜냐하면 강제로 줄여버리면..모션이 끊길것..
 	return (
 		<section id='pics' className='myScroll'>
-			<h1 style={{ transform: `translateX(${currentPos}px)` }}>FLICKR</h1>
+			<h1 style={{ transform: `translateX(${currentPos + 100}px)` }}>FLICKR</h1>
 
 			<article
 				style={{
-					transform: `translate(-50%, -50%) rotate(${Scrolled >= Pos - base ? modifiedPos : 0}deg) scale(${
-						Scrolled >= Pos - base ? 1 + modifiedPos / 500 : 1
-					})`,
-					opacity: `${Scrolled >= Pos - base ? 1 - modifiedPos / 500 : 1}`,
+					height: `${Scrolled >= Pos ? 300 - currentPos : 300}px`,
+					// transform: `translate(-50%, -50%) rotate(${Scrolled >= Pos - base ? modifiedPos : 0}deg) scale(${
+					// 	Scrolled >= Pos - base ? 1 + modifiedPos / 500 : 1
+					// })`,
+					// opacity: `${Scrolled >= Pos - base ? 1 - modifiedPos / 500 : 1}`,
 				}}
-			></article>
+			>
+				<img src={process.env.PUBLIC_URL + '/img/Community.jpg'} alt='' />
+			</article>
 		</section>
 	);
 }

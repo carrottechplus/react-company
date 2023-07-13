5ac0c23ec96e79cd098a9c356081c3a1730cd643
carrottechplus <blohzzang@gmail.com>
2023-07-13 10:46:02 +0900

    reducer 함수 생성

--- src/redux/actionType.js
+++ src/redux/actionType.js
@@ -1,2 +1,7 @@
 // 각 데이터 카테고리 별로 사용될 action 타입 명을  변수처럼 모아놓은 객체
-//
+
+export const Youtube = {
+	start: 'YOUTUBE_START',
+	success: 'YOUTUBE_SUCCESS',
+	fail: 'YOUTUBE_FAIL',
+};
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,1 +1,24 @@
 // 전역 state의 데이터를 action 타입의 종류에 따라서 변형해서 반환하는 함수
+
+import { combineReducers } from 'redux';
+import * as types from './actionType';
+// actionType에 있는 모든 것들 types로 명명
+
+const youtubeReducer = (state = { youtube: [] }, action) => {
+	switch (action.type) {
+		case types.Youtube.start:
+			return state;
+
+		case types.Youtube.success:
+			return { ...state, youtube: action.payload };
+
+		case types.Youtube.fail:
+			return { ...state, youtube: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+const reducers = combineReducers({ youtubeReducer });
+export default reducers;

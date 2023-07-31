6c3094c134d42656ae11dc33c1ceff8cbe3e7c67
carrottechplus <blohzzang@gmail.com>
2023-07-31 11:00:17 +0900

    전역관리할 데이터를 context에 담아서 호출하는 커스텀 hook 생성

--- src/index.js
+++ src/index.js
@@ -2,24 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import departmentReducer from './redux/departmentSlice';
-import menuReducer from './redux/menuSlice';
+import { GlobalProvider } from './hooks/useGlobalContext';
 
-const store = configureStore({
-	reducer: {
-		department: departmentReducer,
-		menu: menuReducer,
-	},
-});
-
-//브라우저 라우터를 쓰는게 맞는데 깃허브에 올리는 제한사항으로 hashrouter를 씀.
 ReactDOM.render(
 	<HashRouter>
-		<Provider store={store}>
+		<GlobalProvider>
 			<App />
-		</Provider>
+		</GlobalProvider>
 	</HashRouter>,
 	document.getElementById('root')
 );
